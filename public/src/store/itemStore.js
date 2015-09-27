54d2380a325c9558331b07b0b3b6f07560bfeeaf
Christopher Pitt <cgpitt@gmail.com>
2015-09-28 08:59:23 +1300

    Deleting assets

--- public/src/store/itemStore.js
+++ public/src/store/itemStore.js
@@ -11,17 +11,29 @@ let _items = {};
  * @desc Adds a gallery item to the store.
  */
 function create(itemData) {
-    _items[itemData.id] = itemData;
+	_items[itemData.id] = itemData;
 }
 
 /**
  * @func destroy
  * @private
- * @param {string} id
+ * @param {int} id
+ * @param {function} callback
  * @desc Removes a gallery item from the store.
  */
-function destroy(id) {
-    delete _items[id];
+function destroy(id, callback) {
+	jQuery.ajax({ // @todo fix this junk
+		'url': _itemStore.delete_url,
+		'data': {
+			'id': id
+		},
+		'dataType': 'json',
+		'method': 'GET',
+		'success': (data) => {
+			delete _items[id];
+			callback && callback();
+		}
+	});
 }
 
 
@@ -33,78 +45,82 @@ function destroy(id) {
  * @desc Updates an item in the store.
  */
 function update(id, itemData) {
-    // TODO:
+	// TODO:
 }
 
 class ItemStore extends EventEmitter {
 
-    /**
-     * @return {object}
-     * @desc Gets the entire collection of items.
-     */
-    getAll() {
-        return _items;
-    }
+	/**
+	 * @return {object}
+	 * @desc Gets the entire collection of items.
+	 */
+	getAll() {
+		return _items;
+	}
 
-    /**
-     * @func getById
-     * @param {string} id
-     * @return {object}
-     */
-    getById(id) {
-        return _items[id];
-    }
+	/**
+	 * @func getById
+	 * @param {string} id
+	 * @return {object}
+	 */
+	getById(id) {
+		return _items[id];
+	}
 
-    /**
-     * @func emitChange
-     * @desc Triggered when something changes in the store.
-     */
-    emitChange() {
-        this.emit(CONSTANTS.ITEM_STORE.CHANGE);
-    }
+	/**
+	 * @func emitChange
+	 * @desc Triggered when something changes in the store.
+	 */
+	emitChange() {
+		this.emit(CONSTANTS.ITEM_STORE.CHANGE);
+	}
 
-    /**
-     * @param {function} callback
-     */
-    addChangeListener(callback) {
-        this.on(CONSTANTS.ITEM_STORE.CHANGE, callback);
-    }
+	/**
+	 * @param {function} callback
+	 */
+	addChangeListener(callback) {
+		this.on(CONSTANTS.ITEM_STORE.CHANGE, callback);
+	}
 
-    /**
-     * @param {function} callback
-     */
-    removeChangeListener(callback) {
-        this.removeListener(CONSTANTS.ITEM_STORE.CHANGE, callback);
-    }
+	/**
+	 * @param {function} callback
+	 */
+	removeChangeListener(callback) {
+		this.removeListener(CONSTANTS.ITEM_STORE.CHANGE, callback);
+	}
 }
 
 let _itemStore = new ItemStore(); // Singleton
 
 galleryDispatcher.register(function (payload) {
-    switch(payload.action) {
-        case CONSTANTS.ITEM_STORE.CREATE:
-            create(payload.data);
-            if (!payload.silent) {
-                _itemStore.emitChange(payload.silent);
-            }
-            break;
+	switch(payload.action) {
+		case CONSTANTS.ITEM_STORE.CREATE:
+			create(payload.data);
 
-        case CONSTANTS.ITEM_STORE.DESTROY:
-            destroy(payload.data.id);
-            if (!payload.silent) {
-                _itemStore.emitChange(payload.silent);
-            }
-            break;
+			if (!payload.silent) {
+				_itemStore.emitChange(payload.silent);
+			}
 
-        case CONSTANTS.ITEM_STORE.UPDATE:
-            update(payload.data.id, payload.data.updates);
-            if (!payload.silent) {
-                _itemStore.emitChange();
-            }
-            break;
-    }
+			break;
 
-    return true; // No errors. Needed by promise in Dispatcher.
+		case CONSTANTS.ITEM_STORE.DESTROY:
+			destroy(payload.data.id, () => {
+				if (!payload.silent) {
+					_itemStore.emitChange(payload.silent);
+				}
+			});
+
+			break;
+
+		case CONSTANTS.ITEM_STORE.UPDATE:
+			update(payload.data.id, payload.data.updates);
+			if (!payload.silent) {
+				_itemStore.emitChange();
+			}
+			break;
+	}
+
+	return true; // No errors. Needed by promise in Dispatcher.
 });
 
 export default _itemStore;
