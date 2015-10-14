5c43551f97b66222675dc42da229824e5daa1446
David Craig <david@silverstripe.com>
2015-10-15 10:08:40 +1300

    Add translations

---
+++ javascript/lang/src/en.js
@@ -0,0 +1,19 @@
+{
+	"AssetGalleryField.FILTER_TITLE_ASC": "title a-z",
+	"AssetGalleryField.FILTER_TITLE_DESC": "title z-a",
+	"AssetGalleryField.FILTER_DATE_DESC": "newest",
+	"AssetGalleryField.FILTER_DATE_ASC": "oldest",
+	"AssetGalleryField.LOADMORE": "Load more",
+	"AssetGalleryField.BACK": "Back",
+	"AssetGalleryField.CONFIRMDELETE": "Are you sure you want to delete this record?",
+	"AssetGalleryField.TYPE": "File type",
+	"AssetGalleryField.SIZE": "Size",
+	"AssetGalleryField.URL": "URL",
+	"AssetGalleryField.CREATED": "First uploaded",
+	"AssetGalleryField.LASTEDIT": "Last changed",
+	"AssetGalleryField.DIM": "Dimensions",
+	"AssetGalleryField.SAVE": "Save",
+	"AssetGalleryField.CANCEL": "Cancel",
+	"AssetGalleryField.TITLE": "Title",
+	"AssetGalleryField.FILENAME": "Filename"
+}
