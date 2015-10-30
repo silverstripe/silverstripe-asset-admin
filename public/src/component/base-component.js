0a20992710d2c530b275532c8e5dba01fac117ee
David Craig <david@silverstripe.com>
2015-10-30 16:22:17 +1300

    Use SilverStripeComponent

--- public/src/component/base-component.js
+++ public/src/component/base-component.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import SilverStripeComponent from 'silverstripe-component';
 
-export default class extends React.Component {
+export default class extends SilverStripeComponent {
 	bind(...methods) {
 		methods.forEach((method) => this[method] = this[method].bind(this));
 	}
