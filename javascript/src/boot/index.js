39659f4fed6aca5cb15717c2cb97fd6c56327ece
scott1702 <scott@silverstripe.com>
2016-02-02 18:06:43 +1300

    Move editorComponent to be handled with redux

--- javascript/src/boot/index.js
+++ javascript/src/boot/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from '../state/configureStore';
-import GalleryComponent from '../containers/gallery-container';
+import GalleryContainer from '../containers/gallery-container';
 import FileBackend from '../backend/file-backend';
 
 function getVar(name) {
@@ -83,7 +83,7 @@ const store = configureStore(); //Create the redux store
 
 ReactDOM.render(
     <Provider store={store}>
-        <GalleryComponent {...props} />
+        <GalleryContainer {...props} />
     </Provider>,
     $('.asset-gallery-component-wrapper')[0]
 );
