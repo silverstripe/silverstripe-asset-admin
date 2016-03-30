f84988091b25c670be57d156a8b2c4e4f4ca9547
scott1702 <scott@silverstripe.com>
2016-03-31 09:49:16 +1300

    Remove outdated bundle-react

--- code/controllers/AssetAdmin.php
+++ code/controllers/AssetAdmin.php
@@ -82,7 +82,6 @@ class AssetAdmin extends LeftAndMain implements PermissionProvider
     {
         parent::init();
 
-        Requirements::javascript(FRAMEWORK_DIR . "/admin/javascript/dist/bundle-react.js");
         Requirements::javascript(ASSET_ADMIN_DIR . "/javascript/AssetAdmin.js");
         Requirements::add_i18n_javascript(ASSET_ADMIN_DIR . '/javascript/lang', false, true);
         Requirements::css(ASSET_ADMIN_DIR . "/javascript/dist/main.css");
