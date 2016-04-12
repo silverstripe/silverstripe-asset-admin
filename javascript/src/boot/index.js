e3938106428b7e4f443e1001ae03927b72b7e91f
Sam Minnee <sam@silverstripe.com>
2016-04-12 17:16:29 +1200

    REFACTOR: Update to use createEndpointFetcher
    asset-admin previously had its own implementation of
    createEndpointFetcher, apiCallerFromEndpoint. Now that
    createEndpointFetcher is part of framework master, we can use that.

--- javascript/src/boot/index.js
+++ javascript/src/boot/index.js
@@ -6,50 +6,6 @@ import configureStore from 'state/configureStore';
 import AssetAdminContainer from 'sections/asset-admin/controller';
 import backend from 'silverstripe-backend';
 
-/**
- * Return an API caller for the given endpoint, using the given backend
- * Returns a lambda that takes an object payload returns a promise.
- * The promise will output another object payload as the argument passed to then()
- */
-function apiCallerFromEndpoint(endpoint) {
-  function serialize(obj) {
-    const str = [];
-    let p;
-    for (p in obj) {
-      if (obj.hasOwnProperty(p)) {
-        str.push(`${encodeURIComponent(p)}=${encodeURIComponent(obj[p])}`);
-      }
-    }
-    return str.join('&');
-  }
-
-  return (data) => {
-    let url = endpoint.url;
-    let backendData = data;
-
-    if (endpoint.method === 'get' && (endpoint.payloadFormat || 'urlencoded') === 'urlencoded') {
-      url += `?${serialize(data)}`;
-      backendData = null;
-    }
-
-    let backendString = '';
-    switch (endpoint.payloadFormat || 'urlencoded') {
-      case 'urlencoded':
-        backendString = serialize(backendData);
-        break;
-      case 'json':
-        backendString = JSON.stringify(backendData);
-        break;
-      default:
-        throw new Error(`Unknown payloadFormat: '${endpoint.payloadFormat}'`);
-
-    }
-
-    return backend[endpoint.method](url, backendString)
-      .then(response => response.json());
-  };
-}
-
 $.entwine('ss', () => {
   $('.asset-gallery-component-wrapper').entwine({
     onadd() {
@@ -58,18 +14,22 @@ $.entwine('ss', () => {
       // Build API callers from the URLs provided to us in the div
       // In time, something like a GraphQL endpoint might be a better way to run
 
-      const filesByParentApi = apiCallerFromEndpoint({
+      const filesByParentApi = backend.createEndpointFetcher({
         method: 'get',
+        payloadFormat: 'querystring',
+        responseFormat: 'json',
         url: this.data('asset-gallery-files-by-parent-url'),
       });
 
-      const deleteApi = apiCallerFromEndpoint({
+      const deleteApi = backend.createEndpointFetcher({
         method: 'delete',
+        payloadFormat: 'urlencoded',
         url: this.data('asset-gallery-delete-url'),
       });
 
-      const addFolderApi = apiCallerFromEndpoint({
+      const addFolderApi = backend.createEndpointFetcher({
         method: 'post',
+        payloadFormat: 'urlencoded',
         url: this.data('asset-gallery-add-folder-url'),
       });
 
