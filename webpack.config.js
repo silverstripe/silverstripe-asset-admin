b572fd46ed518271c00010450163312abeeeeacd
Ingo Schommer <ingo@silverstripe.com>
2016-08-29 23:26:58 +1200

    Switch build system to webpack
    See https://github.com/silverstripe/silverstripe-framework/pull/5918

---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const PATHS = {
+  MODULES: './node_modules',
+  MODULE_JS_SRC: './client/src',
+  MODULE_JS_DIST: './client/dist/js',
+  MODULE_CSS_SRC: './client/src/styles',
+  MODULE_CSS_DIST: './client/dist/styles',
+};
+
+// Used for autoprefixing css properties (same as Bootstrap Aplha.2 defaults)
+const SUPPORTED_BROWSERS = [
+  'Chrome >= 35',
+  'Firefox >= 31',
+  'Edge >= 12',
+  'Explorer >= 9',
+  'iOS >= 8',
+  'Safari >= 8',
+  'Android 2.3',
+  'Android >= 4',
+  'Opera >= 12',
+];
+
+module.exports = {
+  entry: {
+    'bundle': `${PATHS.MODULE_JS_SRC}/bundles/bundle.js`,
+  },
+  resolve: {
+    modulesDirectories: [PATHS.MODULE_JS_SRC, PATHS.MODULES],
+  },
+  output: {
+    path: './client/dist',
+    filename: 'js/[name].js',
+  },
+  externals: {
+    'components/Breadcrumb/Breadcrumb': 'Breadcrumb',
+    'components/FormBuilderModal/FormBuilderModal': 'FormBuilderModal',
+    'components/FormBuilder/FormBuilder': 'FormBuilder',
+    'components/Toolbar/Toolbar': 'Toolbar',
+    'state/breadcrumbs/BreadcrumbsActions': 'BreadcrumbsActions',
+    'deep-freeze-strict': 'DeepFreezeStrict',
+    i18n: 'i18n',
+    jQuery: 'jQuery',
+    'lib/Backend': 'Backend',
+    'lib/Config': 'Config',
+    'lib/Injector': 'Injector',
+    'lib/ReducerRegister': 'ReducerRegister',
+    'lib/Router': 'Router',
+    'lib/ReactRouteRegister': 'ReactRouteRegister',
+    'lib/SilverStripeComponent': 'SilverStripeComponent',
+    'page.js': 'Page',
+    'react-addons-css-transition-group': 'ReactAddonsCssTransitionGroup',
+    'react-addons-test-utils': 'ReactAddonsTestUtils',
+    'react-dom': 'ReactDom',
+    'react-redux': 'ReactRedux',
+    'react-router-redux': 'ReactRouterRedux',
+    'react-router': 'ReactRouter',
+    react: 'React',
+    'redux-thunk': 'ReduxThunk',
+    redux: 'Redux',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|thirdparty)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-object-assign'/* , 'transform-object-rest-spread' */],
+          comments: false,
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract([
+          'css?sourceMap&minimize',
+          'postcss?sourceMap',
+          'resolve-url',
+          'sass?sourceMap',
+        ], {
+          publicPath: '../', // needed because bundle.css is in a subfolder
+        }),
+      },
+      {
+        test: /\.(png|gif|jpg|svg)$/,
+        loader: 'file?name=images/[name].[ext]',
+      },
+      {
+        test: /\.(woff|eot|ttf)$/,
+        loader: 'file?name=fonts/[name].[ext]',
+      },
+    ],
+  },
+  postcss: [
+    autoprefixer({ browsers: SUPPORTED_BROWSERS }),
+  ],
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jQuery',
+      jQuery: 'jQuery',
+      'ss.i18n': 'i18n',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        unused: false,
+        warnings: false,
+      },
+    }),
+    new ExtractTextPlugin('styles/bundle.css', { allChunks: true }),
+  ],
+};
