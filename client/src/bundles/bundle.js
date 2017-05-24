df5a1d3d950fcf5cffdda113383af751799e56a9
Christopher Joe <cjoe@silverstripe.com>
2017-05-24 13:40:58 +1200

    Enhancement shift webpack to use the shared silverstripe config module

--- client/src/bundles/bundle.js
+++ client/src/bundles/bundle.js
@@ -1,7 +1,5 @@
-require('expose?InsertMediaModal!containers/InsertMediaModal/InsertMediaModal');
-require('expose?InsertEmbedModal!components/InsertEmbedModal/InsertEmbedModal');
+require('expose-loader?InsertMediaModal!containers/InsertMediaModal/InsertMediaModal');
+require('expose-loader?InsertEmbedModal!components/InsertEmbedModal/InsertEmbedModal');
 
 require('boot');
 require('entwine/UploadField/UploadFieldEntwine.js');
-
-require('styles/bundle.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,150 +1,58 @@
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const path = require('path');
+const Path = require('path');
+const webpackConfig = require('@silverstripe/webpack-config');
+const {
+  resolveJS,
+  externalJS,
+  moduleJS,
+  pluginJS,
+  moduleCSS,
+  pluginCSS,
+} = webpackConfig;
 
+const ENV = process.env.NODE_ENV;
 const PATHS = {
-  MODULES: './node_modules',
-  MODULE_JS_SRC: './client/src',
-  MODULE_JS_DIST: './client/dist/js',
-  MODULE_CSS_SRC: './client/src/styles',
-  MODULE_CSS_DIST: './client/dist/styles',
+  MODULES: 'node_modules',
+  FILES_PATH: '../',
+  ROOT: Path.resolve(),
+  SRC: Path.resolve('client/src'),
+  DIST: Path.resolve('client/dist'),
+  LEGACY_SRC: Path.resolve('client/src/entwine'),
 };
 
-// Used for autoprefixing css properties (same as Bootstrap Aplha.2 defaults)
-const SUPPORTED_BROWSERS = [
-  'Chrome >= 35',
-  'Firefox >= 31',
-  'Edge >= 12',
-  'Explorer >= 9',
-  'iOS >= 8',
-  'Safari >= 8',
-  'Android 2.3',
-  'Android >= 4',
-  'Opera >= 12',
+const config = [
+  {
+    name: 'js',
+    entry: {
+      bundle: `${PATHS.SRC}/bundles/bundle.js`,
+      TinyMCE_ssmedia: `${PATHS.LEGACY_SRC}/TinyMCE_ssmedia.js`,
+      TinyMCE_ssembed: `${PATHS.LEGACY_SRC}/TinyMCE_ssembed.js`,
+    },
+    output: {
+      path: PATHS.DIST,
+      filename: 'js/[name].js',
+    },
+    devtool: (ENV !== 'production') ? 'source-map' : '',
+    resolve: resolveJS(ENV, PATHS),
+    externals: externalJS(ENV, PATHS),
+    module: moduleJS(ENV, PATHS),
+    plugins: pluginJS(ENV, PATHS),
+  },
+  {
+    name: 'css',
+    entry: {
+      bundle: `${PATHS.SRC}/styles/bundle.scss`,
+    },
+    output: {
+      path: PATHS.DIST,
+      filename: 'styles/[name].css',
+    },
+    devtool: (ENV !== 'production') ? 'source-map' : '',
+    module: moduleCSS(ENV, PATHS),
+    plugins: pluginCSS(ENV, PATHS),
+  },
 ];
 
-module.exports = {
-  entry: {
-    bundle: `${PATHS.MODULE_JS_SRC}/bundles/bundle.js`,
-    TinyMCE_ssmedia: `${PATHS.MODULE_JS_SRC}/entwine/TinyMCE_ssmedia.js`,
-    TinyMCE_ssembed: `${PATHS.MODULE_JS_SRC}/entwine/TinyMCE_ssembed.js`,
-  },
-  resolve: {
-    modulesDirectories: [PATHS.MODULE_JS_SRC, PATHS.MODULES],
-  },
-  output: {
-    path: './client/dist',
-    filename: 'js/[name].js',
-  },
-  externals: {
-    'apollo-client': 'ApolloClient',
-    'redux-form': 'ReduxForm',
-    'containers/InsertMediaModal/InsertMediaModal': 'InsertMediaModal',
-    'components/Breadcrumb/Breadcrumb': 'Breadcrumb',
-    'state/schema/SchemaActions': 'SchemaActions',
-    'components/FieldHolder/FieldHolder': 'FieldHolder',
-    'components/LiteralField/LiteralField': 'LiteralField',
-    'lib/DataFormat': 'DataFormat',
-    'lib/schemaFieldValues': 'schemaFieldValues',
-    'lib/reduxFieldReducer': 'reduxFieldReducer',
-    'components/FormBuilderModal/FormBuilderModal': 'FormBuilderModal',
-    'components/FormBuilder/FormBuilder': 'FormBuilder',
-    'components/Toolbar/Toolbar': 'Toolbar',
-    'components/FormAlert/FormAlert': 'FormAlert',
-    'components/Badge/Badge': 'Badge',
-    'containers/FormBuilderLoader/FormBuilderLoader': 'FormBuilderLoader',
-    'state/breadcrumbs/BreadcrumbsActions': 'BreadcrumbsActions',
-    'deep-freeze-strict': 'DeepFreezeStrict',
-    'graphql-tag': 'GraphQLTag',
-    i18n: 'i18n',
-    jquery: 'jQuery',
-    jQuery: 'jQuery',
-    'lib/Backend': 'Backend',
-    'lib/Config': 'Config',
-    'lib/Injector': 'Injector',
-    'lib/ReducerRegister': 'ReducerRegister',
-    'lib/Router': 'Router',
-    'lib/ReactRouteRegister': 'ReactRouteRegister',
-    'lib/SilverStripeComponent': 'SilverStripeComponent',
-    'page.js': 'Page',
-    'react-addons-css-transition-group': 'ReactAddonsCssTransitionGroup',
-    'react-addons-test-utils': 'ReactAddonsTestUtils',
-    'react-apollo': 'ReactApollo',
-    'react-dom': 'ReactDom',
-    'react-redux': 'ReactRedux',
-    'react-bootstrap-ss': 'ReactBootstrap',
-    'react-router-redux': 'ReactRouterRedux',
-    'react-router': 'ReactRouter',
-    react: 'React',
-    'react-dnd': 'ReactDND',
-    'react-dnd-html5-backend': 'ReactDNDHtml5Backend',
-    'redux-thunk': 'ReduxThunk',
-    redux: 'Redux',
-    qs: 'qs',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|thirdparty)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react'],
-          plugins: ['transform-object-assign'/* , 'transform-object-rest-spread' */],
-          comments: false,
-        },
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract([
-          'css?sourceMap&minimize&-core&discardComments',
-          'postcss?sourceMap',
-          'resolve-url',
-          'sass?sourceMap',
-        ], {
-          publicPath: '../', // needed because bundle.css is in a subfolder
-        }),
-      },
-      {
-        test: /\.(png|gif|jpg|svg)$/,
-        loader: 'file?name=images/[name].[ext]',
-      },
-      {
-        test: /\.(woff|eot|ttf)$/,
-        loader: 'file?name=fonts/[name].[ext]',
-      },
-    ],
-  },
-  sassLoader: {
-    // Non-standard include path for silverstripe/admin module
-    includePaths: [
-      path.resolve(__dirname, 'client/src/styles'),
-      path.resolve(__dirname, 'silverstripe-admin/client/src/styles'),
-      path.resolve(__dirname, '../silverstripe-admin/client/src/styles'),
-    ],
-  },
-  postcss: [
-    autoprefixer({ browsers: SUPPORTED_BROWSERS }),
-  ],
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'ss.i18n': 'i18n',
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        unused: false,
-        warnings: false,
-      },
-      output: {
-        beautify: false,
-        semicolons: false,
-        comments: false,
-        max_line_len: 200,
-      },
-    }),
-    new ExtractTextPlugin('styles/bundle.css', { allChunks: true }),
-  ],
-};
+// Use WEBPACK_CHILD=js or WEBPACK_CHILD=css env var to run a single config
+module.exports = (process.env.WEBPACK_CHILD)
+  ? config.find((entry) => entry.name === process.env.WEBPACK_CHILD)
+  : module.exports = config;
