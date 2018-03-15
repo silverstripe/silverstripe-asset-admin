7ad38f38d39331e8be54e30c716d360b08b50480
Christopher Joe <flamer.ohr@gmail.com>
2018-03-16 12:37:06 +1300

    Move the transform to a separate function

--- client/src/boot/registerQueries.js
+++ client/src/boot/registerQueries.js
@@ -2,6 +2,31 @@ import Injector from 'lib/Injector';
 import { fileInterface, file, folder } from 'lib/fileFragments';
 import { buildQueryConfig as buildCreateFolderConfig } from 'state/files/createFolderMutation';
 
+const forceAllFoldersToBob = (manager) => {
+  manager.transformApolloConfig('props', ({ mutate }) => (prevProps) => {
+    const createFolder = (parentId) => mutate({
+      variables: {
+        folder: {
+          parentId,
+          // this transform forces all folders to be called "another-bob" for no reason at all
+          name: 'another-bob',
+        },
+      },
+    });
+
+    return {
+      ...prevProps,
+      actions: {
+        ...prevProps.actions,
+        files: {
+          ...prevProps.actions.files,
+          createFolder,
+        },
+      },
+    };
+  });
+};
+
 const registerQueries = () => {
   Injector.query.registerFragment('FileInterfaceFields', fileInterface);
   Injector.query.registerFragment('FileFields', file);
@@ -10,31 +35,9 @@ const registerQueries = () => {
   // @todo delete these examples/POCs, CreateFolder is not used in favour of form submit
   // and `FolderCreateInputType` does not exist
   Injector.query.register('CreateFolder', buildCreateFolderConfig());
-  Injector.transform('folder-bob', (updater) => {
-    updater.query('CreateFolder', (manager) => {
-      manager.transformApolloConfig('props', ({ mutate }) => (prevProps) => {
-        const createFolder = (parentId) => mutate({
-          variables: {
-            folder: {
-              parentId,
-              // this transform forces all folders to be called "another-bob" for no reason
-              name: 'another-bob',
-            },
-          },
-        });
 
-        return {
-          ...prevProps,
-          actions: {
-            ...prevProps.actions,
-            files: {
-              ...prevProps.actions.files,
-              createFolder,
-            },
-          },
-        };
-      });
-    });
+  Injector.transform('folder-bob', (updater) => {
+    updater.query('CreateFolder', forceAllFoldersToBob);
   });
 };
 
