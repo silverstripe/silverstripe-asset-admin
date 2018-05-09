4897d2a50753724d2ecaea554981357b6feea566
Aaron Carlino <unclecheese@leftandmain.com>
2018-05-09 19:14:39 +1200

    Refactor resultBehaviors

--- client/src/state/files/deleteFilesMutation.js
+++ client/src/state/files/deleteFilesMutation.js
@@ -1,20 +1,41 @@
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { query as readFilesQuery, config as readFilesConfig } from './readFilesQuery';
 
 const mutation = gql`mutation DeleteFiles($IDs:[ID]!) {
   deleteFiles(IDs: $IDs)
 }`;
 
 const config = {
-  props: ({ mutate, ownProps: { actions } }) => {
-    const deleteFiles = (IDs, dataIds) => mutate({
+  props: ({ mutate, ownProps }) => {
+    const { actions } = ownProps;
+    const deleteFiles = (IDs) => mutate({
       variables: {
         IDs,
       },
-      resultBehaviors: dataIds.map(dataId => ({
-        type: 'DELETE',
-        dataId,
-      })),
+      update: (store, { data: { deleteFiles } }) => {
+        const variables = readFilesConfig.options(ownProps).variables;
+        const data = store.readQuery({ query: readFilesQuery, variables });
+        const newData = {
+          ...data,
+          readFiles: {
+            ...data.readFiles,
+            edges: [
+              {
+                ...data.readFiles.edges[0],
+                node: {
+                  ...data.readFiles.edges[0].node,
+                  children: {
+                    ...data.readFiles.edges[0].node.children,
+                    edges: data.readFiles.edges[0].node.children.edges.filter(edge => !IDs.includes(edge.node.id))
+                  }
+                },
+              },
+            ]
+          }
+        };
+        store.writeQuery({ query: readFilesQuery, data: newData, variables });
+      }
     });
 
     return {
