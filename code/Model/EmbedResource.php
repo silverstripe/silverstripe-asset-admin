9237636bb8f07ee85e4afc8aecab5864dfbccd43
Robbie Averill <robbie@averill.co.nz>
2018-06-20 13:47:16 +1200

    API Deprecate Embeddable and EmbedResource, shift into silverstripe/framework (#795)

--- code/Model/EmbedResource.php
+++ code/Model/EmbedResource.php
@@ -2,78 +2,10 @@
 
 namespace SilverStripe\AssetAdmin\Model;
 
-use Embed\Adapters\Adapter;
-use Embed\Embed;
-use SilverStripe\Core\Manifest\ModuleResourceLoader;
-
 /**
- * Encapsulation of an embed tag, linking to an external media source.
- *
- * @see Embed
+ * @deprecated 1.2..2.0 Use {@link \SilverStripe\View\Embed\EmbedResource} instead
  */
-class EmbedResource implements Embeddable
+class EmbedResource extends \SilverStripe\View\Embed\EmbedResource
 {
-    /**
-     * Embed result
-     *
-     * @var Adapter
-     */
-    protected $embed;
 
-    public function __construct($url)
-    {
-        $this->embed = Embed::create($url);
-    }
-
-    public function getWidth()
-    {
-        return $this->embed->getWidth() ?: 100;
-    }
-
-    public function getHeight()
-    {
-        return $this->embed->getHeight() ?: 100;
-    }
-
-    public function getPreviewURL()
-    {
-        // Use thumbnail url
-        if ($this->embed->image) {
-            return $this->embed->image;
-        }
-
-        // Use direct image type
-        if ($this->getType() === 'photo' && !empty($this->embed->url)) {
-            return $this->embed->url;
-        }
-
-        // Default media
-        return ModuleResourceLoader::resourceURL(
-            'silverstripe/asset-admin:client/dist/images/icon_file.png'
-        );
-    }
-
-    /**
-     * Get human readable name for this resource
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        if ($this->embed->title) {
-            return $this->embed->title;
-        }
-
-        return preg_replace('/\?.*/', '', basename($this->embed->getUrl()));
-    }
-
-    public function getType()
-    {
-        return $this->embed->type;
-    }
-
-    public function validate()
-    {
-        return !empty($this->embed->code);
-    }
 }
