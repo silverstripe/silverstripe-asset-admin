99ff45a818828425ef9765aeac973a8821fd4194
Maxime Rainville <maxime@silverstripe.com>
2019-01-21 17:56:14 +1300

    MINOR Remove ImageBackendFactory Injector override from ImageThumbnailHelper

--- code/Helper/ImageThumbnailHelper.php
+++ code/Helper/ImageThumbnailHelper.php
@@ -3,10 +3,7 @@ namespace SilverStripe\AssetAdmin\Helper;
 
 use SilverStripe\AssetAdmin\Controller\AssetAdmin;
 use SilverStripe\Assets\File;
-use SilverStripe\Assets\ImageBackendFactory;
 use SilverStripe\Core\Injector\Injectable;
-use SilverStripe\Core\Injector\InjectionCreator;
-use SilverStripe\Core\Injector\Injector;
 
 class ImageThumbnailHelper
 {
@@ -15,11 +12,6 @@ class ImageThumbnailHelper
     public function run()
     {
         $assetAdmin = AssetAdmin::singleton();
-        $creator = new InjectionCreator();
-        Injector::inst()->registerService(
-            $creator,
-            ImageBackendFactory::class
-        );
         $files = File::get();
 
         set_time_limit(0);
