1d4beccdf274fb4b0f2702399000ce79a7a7b700
Aaron Carlino <unclecheese@leftandmain.com>
2019-05-01 16:31:03 +1200

    MINOR: add logging to image thumbnail helper

--- code/Helper/ImageThumbnailHelper.php
+++ code/Helper/ImageThumbnailHelper.php
@@ -1,6 +1,7 @@
 <?php
 namespace SilverStripe\AssetAdmin\Helper;
 
+use Psr\Log\LoggerInterface;
 use SilverStripe\AssetAdmin\Controller\AssetAdmin;
 use SilverStripe\Assets\File;
 use SilverStripe\Core\Convert;
@@ -12,10 +13,25 @@ class ImageThumbnailHelper
 {
     use Injectable;
 
+    private static $dependencies = [
+        'logger' => '%$' . LoggerInterface::class . '.quiet',
+    ];
+
+    /** @var LoggerInterface|null */
+    private $logger;
+
     /**
      * @var int
      */
     private $maxImageFileSize;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     /**
      * @param int|string $maxImageSize Maximum file size for which thumbnails will be generated. Set to `0` to disable the
@@ -51,14 +67,30 @@ class ImageThumbnailHelper
         $assetAdmin = AssetAdmin::singleton();
         /** @var File[]|SS_List $files */
         $files = File::get();
+        $totalCount = $files->count();
+        if (!$totalCount) {
+            return 0;
+        }
+
+        $this->logger->info(sprintf('Generating %d thumbnails', $totalCount));
 
         set_time_limit(0);
 
         $maxSize = $this->getMaxImageFileSize();
+        $processedCount = 0;
         foreach ($files as $file) {
+            $this->logger->info(sprintf('Generating thumbnail for %s', $file->Filename));
             if ($maxSize == 0 || $file->getAbsoluteSize() < $maxSize) {
                 $assetAdmin->generateThumbnails($file, true);
             }
+
+            $processedCount++;
+            $this->logger->info(sprintf(
+                '[%d / %d, %d%% complete]',
+                $processedCount,
+                $totalCount,
+                floor(($processedCount / $totalCount) * 100)
+            ));
         }
     }
 }
