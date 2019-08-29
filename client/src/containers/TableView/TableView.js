82a4b16d78997093043b4b4f5f7da22368468249
Maxime Rainville <maxime@silverstripe.com>
2019-08-29 18:40:35 +1200

    BUG Add a status column to the table view

--- client/src/containers/TableView/TableView.js
+++ client/src/containers/TableView/TableView.js
@@ -4,6 +4,7 @@ import Griddle from 'griddle-react';
 import i18n from 'i18n';
 import { galleryViewPropTypes, galleryViewDefaultProps } from 'containers/Gallery/Gallery';
 import { fileSize } from 'lib/DataFormat';
+import VersionedBadge from 'components/VersionedBadge/VersionedBadge';
 
 class TableView extends Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class TableView extends Component {
     this.handleRowClick = this.handleRowClick.bind(this);
     this.renderSelect = this.renderSelect.bind(this);
     this.renderTitle = this.renderTitle.bind(this);
+    this.renderStatus = this.renderStatus.bind(this);
     this.renderNoItemsNotice = this.renderNoItemsNotice.bind(this);
 
     this.state = {
@@ -45,6 +47,7 @@ class TableView extends Component {
     const columns = [
       'thumbnail',
       'title',
+      'status',
       'size',
       'lastEdited',
     ];
@@ -80,18 +83,26 @@ class TableView extends Component {
       {
         columnName: 'title',
         customCompareFn: () => (0), // Suppress griddle re-sorting
+        displayName: i18n._t('File.TITLE', 'Title'),
         cssClassName: 'gallery__table-column--title',
         customComponent: this.renderTitle,
       },
       {
+        columnName: 'status',
+        sortable: false,
+        cssClassName: 'sort--disabled',
+        customComponent: this.renderStatus,
+        displayName: i18n._t('File.STATUS', 'Status'),
+      },
+      {
         columnName: 'lastEdited',
-        displayName: 'Modified',
+        displayName: i18n._t('File.MODIFIED', 'Modified'),
         customComponent: this.renderDate,
       },
       {
         columnName: 'size',
         sortable: false,
-        displayName: 'Size',
+        displayName: i18n._t('File.SIZE', 'Size'),
         cssClassName: 'sort--disabled',
         customComponent: this.renderSize,
       },
@@ -243,6 +254,35 @@ class TableView extends Component {
     return (
       <span>{description}</span>
     );
+  }
+
+  /**
+   * Renders the content for the status column
+   *
+   * @param {object} props
+   * @returns {Component|null}
+   */
+  renderStatus(props) {
+    let flags = [];
+    const item = props.rowData;
+
+    if (item.type !== 'folder') {
+      if (item.draft) {
+        flags.push({
+          key: 'status-draft',
+          status: 'draft'
+        });
+      } else if (item.modified) {
+        flags.push({
+          key: 'status-modified',
+          status: 'modified'
+        });
+      }
+    }
+
+    flags = flags.map(({ ...attributes }) => <VersionedBadge {...attributes} />);
+
+    return <span>{flags}</span>;
   }
 
   /**
