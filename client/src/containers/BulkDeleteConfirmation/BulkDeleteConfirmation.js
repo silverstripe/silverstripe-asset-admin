0b0eafd9f75aa32bd4ca70509415c0d56fc023b8
Maxime Rainville <maxime@silverstripe.com>
2019-09-17 16:48:19 +1200

    Clean up BulkDeleteCOnfirmation and add some unit tests.

--- client/src/containers/BulkDeleteConfirmation/BulkDeleteConfirmation.js
+++ client/src/containers/BulkDeleteConfirmation/BulkDeleteConfirmation.js
@@ -1,130 +1,78 @@
-import i18n from 'i18n';
-import React, { Fragment } from 'react';
+import React from 'react';
 import classnames from 'classnames';
 import { bindActionCreators, compose } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {withApollo} from "react-apollo";
+import { withApollo } from "react-apollo";
 import { inject, injectGraphql } from 'lib/Injector';
 import * as confirmDeletionActions from 'state/confirmDeletion/ConfirmDeletionActions';
-import DeletionModal from '../../components/DeletionModal/DeletionModal';
-
-
-const folderInUseMessage = i18n._t(
-  'AssetAdmin.BULK_ACTIONS_DELETE_FOLDER',
-  'These folders contain files which are currently in use, you must move or delete their contents before you can delete the folder.'
-);
+import DeletionModal from 'components/DeletionModal/DeletionModal';
+import BulkDeleteMessage from './BulkDeleteMessage';
+import { getFileInUseCount, getFolderInUse } from './helpers';
+import fileShape from 'lib/fileShape';
 
 /**
- * Detect if there's any folder in use in our current selection
- * @param {Object[]} files
- * @param {Object} fileUsageData
- * @return {boolean}
+ * Wires the Redux store and Apollo result set with the DeletionModal.
+ *
+ * @todo Translate the action labels
  */
-const foldersInUse = (files, fileUsage) => {
-  const folderInUse = files
-    .filter(({type}) => (type === 'folder'))
-    .filter(({id}) => (fileUsage[id] && fileUsage[id] > 0));
-  return folderInUse.length > 0;
-}
-
-/**
- * Count the number of files that are currently in use and the nomber of places where they are in use.
- * @param {Object[]} files
- * @param {Object} fileUsageData
- * @return {Object}
- */
-const getFileInUseCount = (files, fileUsage) => (
-  files.filter( ({type}) => (type !== 'folder'))
-    .reduce((accumulator, {id}) => {
-      if (fileUsage[id]) {
-        accumulator.fileInUseCount++;
-        accumulator.inUseCount += fileUsage[id];
-      };
-      return accumulator;
-    }, {fileInUseCount: 0, inUseCount: 0})
-);
-
-const confirmationMessage = (fileCount, fileInUseCount, inUseCount) => {
-  if (fileInUseCount === 0) {
-    return i18n._t('AssetAdmin.BULK_ACTIONS_DELETE_CONFIRM', 'Are you sure you want to delete these files?');
-  }
-
-  let msg = '';
-
-  if (fileCount === 1 && fileInUseCount === 1) {
-    msg = i18n.sprintf(
-      i18n._t(
-        'AssetAdmin.BULK_ACTIONS_DELETE_SINGLE_CONFIRM',
-        'This file is currently used in %s place(s), are you sure you want to delete it?'
-      ),
-      inUseCount
-    );
-  } else {
-    msg = i18n.sprintf(
-      i18n._t(
-        'AssetAdmin.BULK_ACTIONS_DELETE_MULTI_CONFIRM',
-        'There are %s files currently in use, are you sure you want to delete these files?'
-      ),
-      fileInUseCount
-    );
-  }
-
-  const warning = i18n._t(
-    'AssetAdmin.BULK_ACTIONS_DELETE_WARNING',
-    'Ensure files are removed from content areas prior to deleting them, otherwise they will appear as broken links.'
-  );
-
-  return (
-    <Fragment>
-      <p>{msg}</p>
-      <p>{warning}</p>
-    </Fragment>
-  );
-}
-
 const BulkDeleteConfirmation = ({
   loading, LoadingComponent, transition,
   files, fileUsage,
-  actions: {confirmDeletion: {cancel, reset} },
-  onConfirm,
-  ...props
+  onCancel, reset, onConfirm
 }) => {
   let body = null;
-  let actions = [];
+  let actions = [{label: 'Dismiss', handler: onCancel, color: 'primary'}]
 
+  // Decide what message and action to show users
   if (loading) {
+    // We're still waiting for results from GraphQL
     body = <LoadingComponent />;
-  } else if (foldersInUse(files, fileUsage)) {
-    body = folderInUseMessage;
-    actions = [{label: 'Dismiss', handler: cancel, color: 'primary'}];
   } else {
-    const {fileInUseCount, inUseCount} = getFileInUseCount(files, fileUsage);
-    body = confirmationMessage(files.length, fileInUseCount, inUseCount);
-    actions = [
-      {label: 'Delete', handler: () => onConfirm(files), color: 'danger'},
-      {label: 'Cancel', handler: cancel},
-    ];
+    const folderInUse = getFolderInUse(files, fileUsage);
+    const fileUsageCount = getFileInUseCount(files, fileUsage)
+
+    const bodyProps = {folderInUse, fileCount: files.length, ...fileUsageCount};
+    body = <BulkDeleteMessage {...bodyProps} />;
+
+    if (folderInUse) {
+      actions = [
+        {label: 'Delete', handler: () => onConfirm(files), color: 'danger'},
+        {label: 'Cancel', handler: onCancel},
+      ];
+    }
   }
 
-  const extraProps = {
-    isOpen: !['canceling', 'deleting'].includes(transition),
-    body
-  };
+  // If we're in the process of canceling/deleting results, we set isOpen to false.
+  // This is just a way to get the nice modal out transtion.
+  // When tell the modal to call the `reset` action when it's done closing.
+  const isOpen = !['canceling', 'deleting'].includes(transition);
 
   return <DeletionModal
-    {...extraProps }
-    actions={ actions }
-    onCancel={ cancel }
-    onClosed={ reset }
+      body={ body }
+      isOpen={ isOpen }
+      actions={ actions }
+      onCancel={ onCancel }
+      onClosed={ reset }
     />
 }
 
-const Component = (props) => {
-  const {showConfirmation, files} = props;
-  return showConfirmation && files.length > 0 ? <ConnectedModal {...props} /> : null ;
-}
+BulkDeleteConfirmation.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  LoadingComponent: PropTypes.func,
+  transition: PropTypes.oneOf(['canceling', 'deleting', false]),
+  files: PropTypes.arrayOf(fileShape),
+  fileUsage: PropTypes.object,
+  onCancel: PropTypes.func.isRequired,
+  reset: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+};
 
+/**
+ * Wires the Modal with the the GraphQL File Usage Query. Results will be provided via a `fileUsage` prop containing a
+ * map of file/folder IDs to the number of location where the file/folder is in use.
+ * e.g.: If you're trying to delete file ID 1234 and it's use in 10 pages you will get `['1234': 10]`.
+ */
 const ConnectedModal = compose(
   inject(
     ['Loading'],
@@ -134,13 +82,22 @@ const ConnectedModal = compose(
   withApollo
 )(BulkDeleteConfirmation);
 
+/**
+ * Decide whether to render the Deletion Modal based on the information in the redux store. This avoid firing off a
+ * GraphQL query for nothing.
+ */
+const ConditionalModal = ({showConfirmation, files, ...props}) => (
+  showConfirmation && files.length > 0 ? <ConnectedModal {...props} files={files} /> : null
+);
+
 const mapStateToProps = ({assetAdmin: {confirmDeletion}}) => confirmDeletion;
-const mapDispatchToProps = (dispatch) =>  ({
-  actions: {
-    confirmDeletion: bindActionCreators(confirmDeletionActions, dispatch)
-  },
-});
+const mapDispatchToProps = {
+  onCancel: confirmDeletionActions.cancel,
+  reset: confirmDeletionActions.reset()
+};
 
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
-)(Component);
+)(ConditionalModal);
+
+export {BulkDeleteConfirmation as Component};
---
+++ client/src/containers/BulkDeleteConfirmation/BulkDeleteMessage.js
@@ -0,0 +1,72 @@
+import i18n from 'i18n';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Choose which message to delete confirmation message to display.
+ * @param {boolean} folderInUse
+ * @param {number} fileCount
+ * @param {number} fileInUseCount
+ * @param {number} inUseCount
+ * @returns {string}
+ */
+const confirmationMessage = (folderInUse, fileCount, fileInUseCount, inUseCount) => {
+  if (folderInUse) {
+    return i18n._t(
+      'AssetAdmin.BULK_ACTIONS_DELETE_FOLDER',
+      'These folders contain files which are currently in use, you must move or delete their contents before ' +
+      'you can delete the folder.'
+    );
+  }
+
+  if (fileInUseCount === 0) {
+    return i18n._t('AssetAdmin.BULK_ACTIONS_DELETE_CONFIRM', 'Are you sure you want to delete these files?');
+  }
+
+  if (fileCount === 1 && fileInUseCount === 1) {
+    return i18n.sprintf(
+      i18n._t(
+        'AssetAdmin.BULK_ACTIONS_DELETE_SINGLE_CONFIRM',
+        'This file is currently used in %s place(s), are you sure you want to delete it?'
+      ),
+      inUseCount
+    );
+  }
+
+  return i18n.sprintf(
+    i18n._t(
+      'AssetAdmin.BULK_ACTIONS_DELETE_MULTI_CONFIRM',
+      'There are %s files currently in use, are you sure you want to delete these files?'
+    ),
+    fileInUseCount
+  );
+}
+
+/**
+ * Display a context dependent confirmation message.
+ */
+const BulkDeleteMessage = ({folderInUse, fileCount, fileInUseCount, inUseCount}) => (
+  <Fragment>
+    <p>{confirmationMessage(folderInUse, fileCount, fileInUseCount, inUseCount)}</p>
+    {!folderInUse && fileInUseCount > 0 && <p>{i18n._t(
+      'AssetAdmin.BULK_ACTIONS_DELETE_WARNING',
+      'Ensure files are removed from content areas prior to deleting them, otherwise they will appear as broken links.'
+    )}</p>}
+  </Fragment>
+);
+
+BulkDeleteMessage.propTypes = {
+  folderInUse: PropTypes.bool,
+  fileCount: PropTypes.number,
+  fileInUseCount: PropTypes.number,
+  inUseCount: PropTypes.number
+};
+
+BulkDeleteMessage.defaultProps = {
+  folderInUse: false,
+  fileCount: 0,
+  fileInUseCount: 0,
+  inUseCount: 0
+}
+
+export default BulkDeleteMessage;
