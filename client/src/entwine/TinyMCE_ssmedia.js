2750a964fefbd14e384b7ae82ccc5e4b6229d04f
Damian Mooyman <damian.mooyman@isobar.com>
2019-09-24 17:23:21 +1200

    FEATURE Enable 'upload_folder_id' config to specify ssmedia upload directory for HTML editor field Fixes #933

--- client/src/entwine/TinyMCE_ssmedia.js
+++ client/src/entwine/TinyMCE_ssmedia.js
@@ -173,6 +173,7 @@ jQuery.entwine('ss', ($) => {
       const handleHide = () => this.close();
       const handleInsert = (...args) => this._handleInsert(...args);
       const attrs = this.getOriginalAttributes();
+      const folderId = this.getFolderId();
       const selection = tinymce.activeEditor.selection;
       const selectionContent = selection.getContent() || '';
       const tagName = selection.getNode().tagName;
@@ -188,6 +189,7 @@ jQuery.entwine('ss', ($) => {
           title={false}
           type="insert-media"
           isOpen={isOpen}
+          folderId={folderId}
           onInsert={handleInsert}
           onClosed={handleHide}
           bodyClassName="modal__dialog"
@@ -237,6 +239,25 @@ jQuery.entwine('ss', ($) => {
         this.close();
       }
       return Promise.resolve();
+    },
+
+    /**
+     * Get default upload folder
+     *
+     * @returns {number}
+     */
+    getFolderId() {
+      const $field = this.getElement();
+      if (!$field) {
+        return null;
+      }
+
+      const config = jQuery($field).data('config');
+      if (typeof config.upload_folder_id === 'number') {
+        return config.upload_folder_id;
+      }
+
+      return null;
     },
 
     /**
