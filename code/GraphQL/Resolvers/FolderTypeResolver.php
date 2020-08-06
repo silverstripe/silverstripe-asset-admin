ea120707a9f71aaae0e5625a8f39d05e41a65966
Aaron Carlino <unclecheese@leftandmain.com>
2020-08-06 13:15:22 +1200

    First pass at graphql 4 compat

---
+++ code/GraphQL/Resolvers/FolderTypeResolver.php
@@ -0,0 +1,114 @@
+<?php
+
+
+namespace SilverStripe\AssetAdmin\GraphQL\Resolvers;
+
+
+use GraphQL\Type\Definition\ResolveInfo;
+use SilverStripe\AssetAdmin\GraphQL\FileFilter;
+use SilverStripe\Assets\File;
+use SilverStripe\Assets\Folder;
+use SilverStripe\GraphQL\Pagination\Connection;
+use SilverStripe\GraphQL\QueryHandler\QueryHandler;
+use SilverStripe\GraphQL\Schema\Resolver\DefaultResolverProvider;
+use SilverStripe\ORM\DataList;
+use SilverStripe\ORM\DataQuery;
+use SilverStripe\ORM\DB;
+use SilverStripe\Versioned\Versioned;
+use InvalidArgumentException;
+use Exception;
+
+class FolderTypeResolver extends DefaultResolverProvider
+{
+    /**
+     * @param Folder $object
+     * @param array $args
+     * @param array $context
+     * @param ResolveInfo $info
+     * @return mixed
+     * @throws InvalidArgumentException
+     * @throws Exception
+     */
+    public function resolveFolderChildren(
+        $object,
+        array $args,
+        $context,
+        ResolveInfo $info
+    ) {
+        // canView() checks on parent folder are implied by the query returning $object
+        // Note: The inability to query permissions against the entire set means pagination
+        // is inaccurate when any item in the list returns false on canView()
+
+        $filter = (!empty($args['filter'])) ? $args['filter'] : [];
+
+        if (isset($filter['parentId']) && (int)$filter['parentId'] !== (int)$object->ID) {
+            throw new InvalidArgumentException(sprintf(
+                'The "parentId" value (#%d) needs to match the current object id (#%d)',
+                (int)$filter['parentId'],
+                (int)$object->ID
+            ));
+        }
+
+        /** @var DataList $list */
+        $list = Versioned::get_by_stage(File::class, 'Stage');
+        $filter['parentId'] = $object->ID;
+        $list = FileFilter::filterList($list, $filter);
+
+
+        // Ensure that we're looking at a subset of relevant data.
+        if (!isset($args['sort'])) {
+            // only show folders first if no manual ordering is set
+
+            $list = $list->alterDataQuery(static function (DataQuery $dataQuery) {
+                $query = $dataQuery->query();
+                $existingOrderBys = [];
+                foreach ($query->getOrderBy() as $field => $direction) {
+                    if (strpos($field, '.') === false) {
+                        // some fields may be surrogates added by extending augmentSQL
+                        // we have to preserve those expressions rather than auto-generated names
+                        // that SQLSelect::addOrderBy leaves for them (e.g. _SortColumn0)
+                        $field = $query->expressionForField(trim($field, '"')) ?: $field;
+                    }
+
+                    $existingOrderBys[$field] = $direction;
+                }
+
+                // Folders should always go first
+                $dataQuery->sort(
+                    sprintf(
+                        '(CASE WHEN "ClassName"=%s THEN 1 ELSE 0 END)',
+                        DB::get_conn()->quoteString(Folder::class)
+                    ),
+                    'DESC',
+                    true
+                );
+
+                foreach ($existingOrderBys as $field => $dir) {
+                    $dataQuery->sort($field, $dir, false);
+                }
+
+                return $dataQuery;
+            });
+        }
+
+        // Filter by permission
+        // DataQuery::column ignores surrogate sorting fields
+        // see https://github.com/silverstripe/silverstripe-framework/issues/8926
+        // the following line is a workaround for `$ids = $list->column('ID');`
+        $ids = $list->dataQuery()->execute()->column('ID');
+
+        $permissionChecker = File::singleton()->getPermissionChecker();
+        $canViewIDs = array_keys(array_filter($permissionChecker->canViewMultiple(
+            $ids,
+            $context[QueryHandler::CURRENT_USER]
+        )));
+        // Filter by visible IDs (or force empty set if none are visible)
+        // Remove the limit as it no longer applies. We've already filtered down to the exact
+        // IDs we need.
+        $canViewList = $list->filter('ID', $canViewIDs ?: 0)
+            ->limit(null);
+
+        return $canViewList;
+    }
+
+}
---
+++ code/GraphQL/Resolvers/PublicationResolver.php
@@ -0,0 +1,166 @@
+<?php
+
+
+namespace SilverStripe\AssetAdmin\GraphQL\Resolvers;
+
+
+use GraphQL\Type\Definition\ResolveInfo;
+use SilverStripe\AssetAdmin\GraphQL\Notice;
+use SilverStripe\Assets\File;
+use SilverStripe\GraphQL\QueryHandler\QueryHandler;
+use SilverStripe\GraphQL\Schema\Resolver\DefaultResolverProvider;
+use SilverStripe\Versioned\RecursivePublishable;
+use SilverStripe\Versioned\Versioned;
+use InvalidArgumentException;
+
+class PublicationResolver extends DefaultResolverProvider
+{
+    const ACTION_PUBLISH = 'publish';
+    const ACTION_UNPUBLISH = 'unpublish';
+
+    public static function resolvePublishFiles(...$params)
+    {
+        return self::resolvePublicationOperation(self::ACTION_PUBLISH, ...$params);
+    }
+
+    public static function resolveUnpublishFiles(...$params)
+    {
+        return self::resolvePublicationOperation(self::ACTION_UNPUBLISH, ...$params);
+    }
+
+    /**
+     * @param string $action
+     * @param mixed $object
+     * @param array $args
+     * @param mixed $context
+     * @param ResolveInfo $info
+     * @return array
+     */
+    private static function resolvePublicationOperation(
+        $action,
+        $object,
+        array $args,
+        $context,
+        ResolveInfo $info
+    ) {
+        if (!isset($args['IDs']) || !is_array($args['IDs'])) {
+            throw new InvalidArgumentException('IDs must be an array');
+        }
+        $isPublish = $action === self::ACTION_PUBLISH;
+        $sourceStage = $isPublish ? Versioned::DRAFT : Versioned::LIVE;
+        $force = $args['force'] ?? false;
+        $quiet = $args['quiet'] ?? false;
+        $result = [];
+        $warningMessages = [];
+        $idList = $args['IDs'];
+        $files = Versioned::get_by_stage(File::class, $sourceStage)
+            ->byIds($idList);
+
+        // If warning suppression is not on, bundle up all the warnings into a single exception
+        if (!$quiet && $files->count() < count($idList)) {
+            $missingIds = array_diff($idList, $files->column('ID'));
+            foreach ($missingIds as $id) {
+                $warningMessages[] = sprintf(
+                    'File #%s either does not exist or is not on stage %s.',
+                    $id,
+                    $sourceStage
+                );
+            }
+        }
+        $allowedFiles = [];
+        // Check permissions
+        foreach ($files as $file) {
+            $permissionMethod = $isPublish ? 'canPublish' : 'canUnpublish';
+            if ($file->$permissionMethod($context[QueryHandler::CURRENT_USER])) {
+                $allowedFiles[] = $file;
+            } elseif (!$quiet) {
+                $warningMessages[] = sprintf(
+                    'User does not have permission to perform this operation on file "%s"',
+                    $file->Title
+                );
+            }
+        }
+
+        if (!empty($warningMessages)) {
+            throw new InvalidArgumentException(implode('\n', $warningMessages));
+        }
+
+        foreach ($allowedFiles as $file) {
+            $result[] = $isPublish
+                ? self::unpublishFile($file, $force)
+                : self::publishFile($file, $force);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param File $file
+     * @param boolean $force
+     * @return File|Notice
+     */
+    private static function publishFile(File $file, $force = false)
+    {
+        $file->publishRecursive();
+
+        return $file;
+    }
+
+
+    /**
+     * @param File|RecursivePublishable $file
+     * @param boolean $force
+     * @return Notice|File
+     */
+    private static function unpublishFile(File $file, $force = false)
+    {
+        // If not forcing, make sure we aren't interfering with any owners
+        if (!$force) {
+            $ownersCount = self::countLiveOwners($file);
+            if ($ownersCount) {
+                return new Notice(
+                    _t(
+                        __CLASS__ . '.OWNER_WARNING',
+                        'File "{file}" is used in {count} place|File "{file}" is used in {count} places.',
+                        [
+                            'file' => $file->Title,
+                            'count' => $ownersCount
+                        ]
+                    ),
+                    'HAS_OWNERS',
+                    [$file->ID]
+                );
+            }
+        }
+
+        $file->doUnpublish();
+        return $file;
+    }
+
+    /**
+     * Count number of live owners this file uses
+     *
+     * @param File $file
+     * @return int Number of live owners
+     */
+    private static function countLiveOwners(File $file): int
+    {
+        // In case no versioning
+        if (!$file->hasExtension(RecursivePublishable::class)) {
+            return 0;
+        }
+
+        // Query live record
+        /** @var Versioned|RecursivePublishable $liveRecord */
+        $liveRecord = Versioned::get_by_stage(File::class, Versioned::LIVE)->byID($file->ID);
+        if ($liveRecord) {
+            return $liveRecord
+                ->findOwners(false)
+                ->count();
+        }
+
+        // No live record, no live owners
+        return 0;
+    }
+
+}
