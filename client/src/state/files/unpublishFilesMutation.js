5187f52e138f0aa498ecd6c596744ef9f9c93da5
Aaron Carlino <unclecheese@leftandmain.com>
2020-10-27 16:05:57 +1300

    New graphql 3 compat

--- client/src/state/files/unpublishFilesMutation.js
+++ client/src/state/files/unpublishFilesMutation.js
@@ -1,12 +1,7 @@
 import { graphql } from 'react-apollo';
 import buildPublicationMutation from './buildPublicationMutation';
-import buildPublicationMutationLegacy from './_legacy/buildPublicationMutation';
 
-// Backward compatibility hack. Remove when GraphQL 4 is in core
-const isLegacy = !!document.body.getAttribute('data-graphql-legacy');
-const builder = isLegacy ? buildPublicationMutationLegacy : buildPublicationMutation;
-
-const { mutation, config } = builder('unpublishFiles');
+const { mutation, config } = buildPublicationMutation('unpublishFiles');
 
 export { mutation, config };
 export default graphql(mutation, config);
