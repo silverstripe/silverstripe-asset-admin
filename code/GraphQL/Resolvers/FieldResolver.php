214bc7b00c9b8868069ca806057369849a52a8ae
Aaron Carlino <unclecheese@leftandmain.com>
2020-10-29 22:53:33 +1300

    Ensure correct graphqlphp for BC

--- code/GraphQL/Resolvers/FieldResolver.php
+++ code/GraphQL/Resolvers/FieldResolver.php
@@ -10,7 +10,11 @@ class FieldResolver
 {
     public static function resolve($obj, array $args, array $context, ResolveInfo $info)
     {
-        $fieldName = FieldAccessor::singleton()->normaliseField($obj, $info->fieldName);
+        $field = $info->fieldName;
+        if ($obj->hasMethod($field)) {
+            return $obj->$field();
+        }
+        $fieldName = FieldAccessor::singleton()->normaliseField($obj, $field);
         return $obj->$fieldName;
     }
 }
