e1caa97560b77e64774a38c576335b2037beb329
Aaron Carlino <unclecheese@leftandmain.com>
2020-11-13 10:53:25 +1300

    Remove commented code, update admin constraint

--- code/GraphQL/Schema/Builder.php
+++ code/GraphQL/Schema/Builder.php
@@ -28,19 +28,6 @@ class Builder implements SchemaUpdater
             $categoryValues[$newKey] = $v;
         }
 
-//        $appCategories = array_keys(File::config()->get('app_categories'));
-//        // Build the app category type
-//        $categoryValues = [];
-//        foreach ($appCategories as $category) {
-//            $categoryValues[$category] = $category;
-//        }
-//        // Sanitise GraphQL Enum aliases (some contain slashes)
-//        foreach ($categoryValues as $key => $v) {
-//            unset($categoryValues[$key]);
-//            $newKey = strtoupper(preg_replace('/[^[[:alnum:]]]*/', '', $key));
-//            $categoryValues[$newKey] = $v;
-//        }
-
         $schema->addEnum(Enum::create('AppCategory', $categoryValues));
     }
 }
