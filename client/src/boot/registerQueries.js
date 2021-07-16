b7061ce8f51b2035026e8dce1504b13bbfce7153
Maxime Rainville <maxime@silverstripe.com>
2021-07-16 23:06:18 +1200

    BUG Update Editor to fetch file info directly from GraphQL so the editor works even when viewing a file not on the current page

--- client/src/boot/registerQueries.js
+++ client/src/boot/registerQueries.js
@@ -1,7 +1,9 @@
 import Injector from 'lib/Injector';
 import { fileInterface, file } from 'lib/fileFragments';
 import readFilesQuery from 'state/files/readFilesQuery';
+import readOneFileQuery from 'state/files/readOneFileQuery';
 import readFilesQueryLegacy from 'state/files/_legacy/readFilesQuery';
+import readOneFileQueryLegacy from 'state/files/_legacy/readOneFileQuery';
 import readFileUsageQuery from 'state/files/readFileUsageQuery';
 
 // Backward compatibility hack. Remove when GraphQL 4 is in core
@@ -11,6 +13,7 @@ const registerQueries = () => {
   Injector.query.registerFragment('FileInterfaceFields', fileInterface);
   Injector.query.registerFragment('FileFields', file);
   Injector.query.register('ReadFilesQuery', isLegacy ? readFilesQueryLegacy : readFilesQuery);
+  Injector.query.register('ReadOneFileQuery', isLegacy ? readOneFileQueryLegacy : readOneFileQuery);
   Injector.query.register('readFileUsageQuery', readFileUsageQuery);
 };
 export default registerQueries;
