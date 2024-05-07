8474c60a73876836ef35ea755e1470d87454f4a2
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-05-07 13:36:54 +1200

    TLN Update translations

--- client/lang/eo.js
+++ client/lang/eo.js
@@ -9,6 +9,21 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
     "AssetAdmin.ADD_FOLDER_BUTTON": "Aldoni dosierujon",
     "AssetAdmin.BACK": "Reen",
     "AssetAdmin.BACK_DESCRIPTION": "Navigi supren unu nivelon",
-    "AssetAdmin.BULK_ACTIONS_ARCHIVE": "Arkivo"
+    "AssetAdmin.BULK_ACTIONS_ARCHIVE": "Arkivo",
+    "AssetAdmin.BULK_ACTIONS_ARCHIVE_FOLDERS_CONFIRM": "Ĉu vi certas ke vi volas enarkivigi ĉi tiujn dosierujojn?",
+    "AssetAdmin.BULK_ACTIONS_ARCHIVE_FOLDER_CONFIRM": "Ĉu vi certas ke vi volas enarkivigi ĉi tiun dosierujon?",
+    "AssetAdmin.BULK_ACTIONS_ARCHIVE_ITEMS_CONFIRM": "Vi estas arkivigonta %s dosiero(j)n kiuj estos uzeblaj en la enhavo de via retejo. Zorge kontrolu la dosieran uzmanieron de la dosieroj antaŭ ol enarkivigi la dosiero(j)n.",
+    "AssetAdmin.BULK_ACTIONS_ARCHIVE_WARNING": "Estu certa ke la dosieroj foriĝis el la enhavaj areoj antaŭ ol enarkivigi ilin, se ne ili restos kiel rompitaj ligiloj.",
+    "AssetAdmin.BULK_ACTIONS_CLEAR_SELECTION": "Vakigu elektaĵon",
+    "AssetAdmin.BULK_ACTIONS_DELETE_FAIL_02": "%s dosierujoj/dosieroj sukcese enarkiviĝis, sed ne povis enarkiviĝi %s dosieroj.",
+    "AssetAdmin.BULK_ACTIONS_DELETE_FOLDERS_CONFIRM": "Ĉu vi certas ke vi volas forigi ĉi tiujn dosierujojn?",
+    "AssetAdmin.BULK_ACTIONS_DELETE_FOLDER_CONFIRM": "Ĉu vi certas ke vi volas forigi ĉi tiun dosierujon?",
+    "AssetAdmin.BULK_ACTIONS_DELETE_ITEMS_CONFIRM": "Vi estas forigonta %s dosiero(j)n kiu(j) estos uzeblaj en la enhavo de via retejo. Zorge kontrolu la dosieran uzmanieron de la dosieroj antaŭ ol forigi la dosiero(j)n.",
+    "AssetAdmin.BULK_ACTIONS_DELETE_SUCCESS_02": "%s dosierujoj/dosieroj sukcese foriĝis.",
+    "AssetAdmin.BULK_ACTIONS_DELETE_WARNING": "Estu certa ke la dosieroj foriĝis el la enhavaj areoj antaŭ ol forigi ilin, se ne ili restos kiel rompitaj ligiloj.",
+    "AssetAdmin.BULK_ACTIONS_SELECTED": "%s elektita",
+    "AssetAdmin.BULK_ACTIONS_SELECT_ALL": "Elekti ĉiujn",
+    "AssetAdmin.CONFIRM_FILE_ARCHIVE": "Konfirmi enarkivigi",
+    "AssetAdmin.CONFIRM_FILE_DELETION": "Konfirmi forigi"
 });
 }
